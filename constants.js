a5e4e2298945f411eb71c77855effbf2b82e6607
David Malaschonok <davidmalaschonok@gmail.com>
2017-04-05 21:41:55 +0200

    Made constants more usable.

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 module.exports = {
-    answer: 'FRESSE!',
-    logSize: 4,
+    answer: 'Würden Sie es mir übel nehmen, sollte ich Sie darum bitten Ihre Fresse zu halten?',
+    logSize: 64,
     threshold: 2,
     token: process.env.TOKEN,
 };
