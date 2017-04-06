ba72fe79e1338b75be612d1111ed373570295813
David Malaschonok <davidmalaschonok@gmail.com>
2017-04-06 12:00:25 +0200

    c

--- answerAndKick.js
+++ answerAndKick.js
@@ -3,15 +3,24 @@ var logSymbols = require('log-symbols');
 
 module.exports = function answerAndKick(msg, answer) {
     // let them know, why they get kicked
-    this.sendMessage(msg.chat.id, answer).catch(function (error) {
-        // bot was probably removed
-        console.log(chalk.red(logSymbols.error, error));
-        return;
-    });
+    this.sendMessage(msg.chat.id, answer).catch(
+        function (error) {
+            // bot was probably removed
+            console.log(chalk.red(logSymbols.error, error));
+            return;
+        }
+    );
     this.kickChatMember(msg.chat.id, msg.from.id).then(function () {
         console.log(chalk.yellow(logSymbols.warning, msg.from.first_name, msg.from.lastname, 'was kicked.'));
     }, function (error) {
         // user is probably admin
         console.log(chalk.red(logSymbols.error, error));
     });
+    // TODO: Ab hier!
+    timers.setTimeout(
+        function () {
+            this.sendMessage(msg.from.id, answer);
+        },
+        banTime * 1000
+    );
 };
--- constants.js
+++ constants.js
@@ -1,5 +1,7 @@
 module.exports = {
     answer: 'Würden Sie es mir übel nehmen, sollte ich Sie darum bitten Ihre Fresse zu halten?',
+    banTime: 10, // seconds
+    invitation: 'Hier könnte Ihre Werbung stehen.',
     logSize: 64,
     threshold: 2,
     token: process.env.TOKEN,
