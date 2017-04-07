915b294bdb0854a0bba25e6c562c4d58aca545ef
Roman KR8 <rkr8@users.noreply.github.com>
2017-04-07 18:21:21 +0200

    Revert "Rejoin and privacy"

--- invite.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (msg) {
-    this.sendMessage(msg.chat.id, this.constants.invitationText).catch(
-        function (error) {
-            // bot was probably removed
-            console.log(chalk.red(logSymbols.error, error));
-            return;
-        }
-    );
-}
--- messageLog.js
+++ messageLog.js
@@ -1,6 +1,5 @@
 module.exports = function (size) {
     return {
-        banned: [],
         logSize: size,
         index: 0,
         messages: new Array(size),
@@ -8,10 +7,6 @@ module.exports = function (size) {
             // minimal ring buffer implementation
             this.messages[this.index] = msg;
             this.index = (this.index + 1) % this.logSize;
-        },
-        addBanned: function (id) {
-            var date = new Date()/1000;
-            this.banned.push([id, date]);
         }
     }
 }
